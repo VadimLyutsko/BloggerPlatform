b1585217f4a8870bfebf81e8fe86a276a970cd0b
vadim <vadim_lyutsko@mail.ru>
2023-02-08 13:42:11 +0300

    Refactored some CSS

--- src/Blogs/Blogs.tsx
+++ src/Blogs/Blogs.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styles from './Blogs.module.css';
 import {SuperSearch} from '../SuperComponents/SuperSearch/SuperSearch';
-import {SuperSort} from '../SuperComponents/SuperSort/SuperSort';
+import {SuperSelect} from '../SuperComponents/SuperSort/SuperSort';
 import {Posts} from '../Posts/Posts';
 
 export const Blogs = () => {
+    const options = ['Old blogs first', 'From A to Z', 'From Z to A'];
     return (
         <main className={styles.blogs}>
             <h3>Blogs</h3>
             <hr/>
             <div className={styles.SearchAndSortPanel}>
                 <SuperSearch/>
-                <SuperSort/>
+                <SuperSelect options={options}/>
             </div>
             <Posts/>
+            <div>
+                <button className={styles.showMoreButton}>Show more 🡇</button>
+            </div>
         </main>
     );
 };
--- src/SuperComponents/SuperSort/SuperSort.tsx
+++ src/SuperComponents/SuperSort/SuperSort.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import styles from './SuperSort.module.css'
+import styles from './SuperSort.module.css';
 
-export const SuperSort = () => {
+type SuperSelectPropsType = {
+    options: string[]
+}
+
+export const SuperSelect: React.FC<SuperSelectPropsType> = ({options}) => {
     return (
         <div className={styles.sortSelect}>
-            <select name="sort" >
-                <option value="JS" >Old blogs first</option>
-                <option value="Java">From A to Z</option>
-                <option value="C#">From Z to A</option>
+            <select name="sort">
+                {options.map(o => {
+                    return <option className={styles.option} value={o}>{o}</option>;
+                })}
             </select>
-
         </div>
     );
 };
